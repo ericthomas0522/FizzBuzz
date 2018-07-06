782b3dc8a7744b5db500644c030bdbda6e239027
Eric Thomas <eric.therock@gmail.com>
2018-07-06 18:20:34 -0500

    My take on solving the fizz buzz problem

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -10,6 +10,29 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
+
+            for (int x = 1; x <= 100; x++)
+                {
+                    if (x % 3 != 0 & x % 5 != 0)
+                         {
+                            Console.WriteLine(x);
+                         }
+
+                     if (x % 3 == 0 & x % 5 != 0)
+                        {
+                             Console.WriteLine("Fizz");
+                         }
+
+                     if (x % 5 == 0 & x % 3 != 0)
+                         {
+                            Console.WriteLine("Buzz");
+                            }
+                    if (x % 5 == 0 & x % 3 == 0)
+                     {
+                    Console.WriteLine("FizzBuzz");
+                    Console.WriteLine();
+                        }
+            }
         }
     }
 }
